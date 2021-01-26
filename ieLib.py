496ea516abe4d56c01cfc005193baca7c57682e3
RabbitHol3 <rilckgg@gmail.com>
2021-01-26 08:30:41 -0300

    Frist

--- ieLib.py
+++ ieLib.py
@@ -7,14 +7,19 @@ class IE:
         self.driver = Dispatch("InternetExplorer.Application")
         self.driver.Visible = kwargs.get('visible', 0)
         self.url = kwargs.get('url', '')
+        self.timeout = kwargs.get('timeout', 5)
 
-    def navigate(self, url, timeout=5):
-        self.driver.Navigate(url)
+    def wait_its_ready(self):
+        timeout = self.timeout
         while self.driver.ReadyState != 4:
             time.sleep(0.05)
             if not timeout:
-                return False
+                raise TimeoutError
             timeout -= 0.05
+
+    def navigate(self, url, timeout=5):
+        self.driver.Navigate(url)
+        self.wait_its_ready()
         return True
 
     def get_element_by_id(self, id):
@@ -42,6 +47,16 @@ class IE:
         for elemento in self.driver.document.all:
             try:
                 if elemento.name == name:
+                    retorno.append(elemento)
+            except AttributeError:
+                continue
+        return retorno
+
+    def get_elements_by_class_name(self, name):
+        retorno = []
+        for elemento in self.driver.document.all:
+            try:
+                if elemento.className == name:
                     retorno.append(elemento)
             except AttributeError:
                 continue
--- main.py
+++ main.py
@@ -2,5 +2,10 @@ from ieLib import IE
 
 ie = IE(visible=True)
 
-ie.navigate('URL')
+ie.navigate('xxx')
+ie.get_elements_by_name('username')[1].value = 'xxx'
+ie.get_elements_by_name('password')[0].value = 'xxx'
+ie.get_elements_by_class_name('btn btn-large btn-inverse btn-block')[0].click()
+ie.wait_its_ready()
 
+#14a86fbc0091
